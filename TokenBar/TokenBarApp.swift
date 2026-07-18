9fc7aa76fd0a9a42077d67a83a147f39c3e9f6b3
Franky <frankylau96@hotmail.com>
2026-07-18 16:21:30 +0800

    Initial Commit

---
+++ TokenBar/TokenBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenBarApp.swift
+//  TokenBar
+//
+//  Created by Frank on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TokenBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
